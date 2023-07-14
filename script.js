1c8808f2e8365557178e896dc13c0cb11a11b64b
benedicti0n <lbandopadhyay2@gmail.com>
2023-07-14 18:10:26 +0530

    hamburger menu

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-gsap.to("#nav",{
-    backgroundColor: "#265B15",
-    height: "80px",
-    duration: 0.5,
-    scrollTrigger:{
-        trigger: "#nav",
-        scroller: "body",
-        start: "top -10%",
-        end: "top -11%",
-        scrub: 2
-    }
-})
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+  // alert("Plz Subscribe ");
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
 
 gsap.to("#main",{
     backgroundColor: "#BC4B48",
